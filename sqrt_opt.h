0daa2f99c6470c6a7dc3fb3fe7620d5194743564
dnszlr <dennis.zhukki@gmail.com>
2022-05-21 19:31:56 +0200

    removed std::cout

--- sqrt_opt.h
+++ sqrt_opt.h
@@ -1,7 +1,6 @@
 #ifndef SQRT_OPT_H
 #define SQRT_OPT_H
 
-// https://gcc.gnu.org/onlinedocs/gcc-4.6.1/gcc/Vector-Extensions.html
 typedef float v4sf __attribute__ ((vector_size (16)));
 typedef int v4si __attribute__ ((vector_size (16)));
 
@@ -9,8 +8,6 @@ typedef int v4si __attribute__ ((vector_size (16)));
 template <size_t LOOPS = 2>
 float sqrt1(float * a) {
   float root;
-  // from here
-  // TODO: your code
   int * ai = reinterpret_cast<int *>(a);
   int * initial = reinterpret_cast<int *>(&root);
   * initial = (1 << 29) + (* ai >> 1) - (1 << 22) - 0x4C000;
@@ -19,14 +16,11 @@ float sqrt1(float * a) {
     root = 0.5 * (root + (* a / root));
   }
   // std::cout << "Square of " << * a << " is: " << root << std::endl;
-  // to here
   return root;
 }
 
 template <size_t LOOPS = 2>
 void sqrt2(float * __restrict__ a, float * __restrict__ root) {
-  // from here
-  // TODO: your code
   for(size_t i = 0; i < 4; i++) {
     int * ai = reinterpret_cast<int *>(&a[i]);
     int * initial = reinterpret_cast<int *>(&root[i]);
@@ -40,14 +34,11 @@ void sqrt2(float * __restrict__ a, float * __restrict__ root) {
     root[3] = 0.5 * (root[3] + (a[3] / root[3]));
   }
   // std::cout << "Sqrt of " << a[3] << " is: " << root[3] << std::endl;
-  // to here
 }
 
 
 template <size_t LOOPS = 2>
 void v4sf_sqrt(v4sf *  __restrict__  a, v4sf *  __restrict__  root) {
-  // from here
-  // TODO: your code
   v4si * ai = reinterpret_cast<v4si *>(a);
   v4si * initial = reinterpret_cast<v4si *>(root);
   * initial = (1 << 29) + (* ai >> 1) - (1 << 22) - 0x4C000;
@@ -63,7 +54,6 @@ void v4sf_sqrt(v4sf *  __restrict__  a, v4sf *  __restrict__  root) {
   std::cout << "Sqrt of " << aF[2] << " is: " << rootF[2] << std::endl;
   std::cout << "Sqrt of " << aF[3] << " is: " << rootF[3] << std::endl;
   **/
-  // to here
 }
 
 
