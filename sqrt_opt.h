0d90f5ece44c89c15caffc0e9f89efaf457e5ca8
dnszlr <dennis.zhukki@gmail.com>
2022-05-24 18:12:10 +0200

    cout update

--- sqrt_opt.h
+++ sqrt_opt.h
@@ -15,7 +15,7 @@ float sqrt1(float * a) {
   for(size_t i = 0; i < LOOPS; i++) {
     root = 0.5 * (root + (* a / root));
   }
-  // std::cout << "Square of " << * a << " is: " << root << std::endl;
+  // std::cout << "Sqrt1 of " << * a << " is: " << root << std::endl;
   return root;
 }
 
@@ -33,12 +33,12 @@ void sqrt2(float * __restrict__ a, float * __restrict__ root) {
     root[2] = 0.5 * (root[2] + (a[2] / root[2]));
     root[3] = 0.5 * (root[3] + (a[3] / root[3]));
   }
-  // std::cout << "Sqrt of " << a[3] << " is: " << root[3] << std::endl;
+  // std::cout << "Sqrt2 of " << a[3] << " is: " << root[3] << std::endl;
 }
 
 
 template <size_t LOOPS = 2>
-void v4sf_sqrt(v4sf *  __restrict__  a, v4sf *  __restrict__  root) {
+void v4sf_sqrt(v4sf * __restrict__ a, v4sf * __restrict__ root) {
   v4si * ai = reinterpret_cast<v4si *>(a);
   v4si * initial = reinterpret_cast<v4si *>(root);
   * initial = (1 << 29) + (* ai >> 1) - (1 << 22) - 0x4C000;
@@ -46,14 +46,12 @@ void v4sf_sqrt(v4sf *  __restrict__  a, v4sf *  __restrict__  root) {
   for (size_t i = 0; i < LOOPS; i++) {
     * root = 0.5 * (* root + (* a / * root));
   }
-  /**
-  float * aF = reinterpret_cast<float *>(a);
-  float * rootF = reinterpret_cast<float *>(root);
-  std::cout << "Sqrt of " << aF[0] << " is: " << rootF[0] << std::endl;
-  std::cout << "Sqrt of " << aF[1] << " is: " << rootF[1] << std::endl;
-  std::cout << "Sqrt of " << aF[2] << " is: " << rootF[2] << std::endl;
-  std::cout << "Sqrt of " << aF[3] << " is: " << rootF[3] << std::endl;
-  **/
+  // float * aF = reinterpret_cast<float *>(a);
+  // float * rootF = reinterpret_cast<float *>(root);
+  // std::cout << "Sqrt3 of " << aF[0] << " is: " << rootF[0] << std::endl;
+  // std::cout << "Sqrt3 of " << aF[1] << " is: " << rootF[1] << std::endl;
+  // std::cout << "Sqrt3 of " << aF[2] << " is: " << rootF[2] << std::endl;
+  // std::cout << "Sqrt3 of " << aF[3] << " is: " << rootF[3] << std::endl;
 }
 
 
