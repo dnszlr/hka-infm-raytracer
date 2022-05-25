1bbe938d2d76da06291b780f4b8c67adb81f89f1
dnszlr <dennis.zhukki@gmail.com>
2022-05-25 14:53:18 +0200

    update

--- sqrt_opt.h
+++ sqrt_opt.h
@@ -11,8 +11,7 @@ float sqrt1(float * a) {
   int * ai = reinterpret_cast<int *>(a);
   int * initial = reinterpret_cast<int *>(&root);
   * initial = (1 << 29) + (* ai >> 1) - (1 << 22) - 0x4C000;
-  root = * reinterpret_cast<float *>(initial);
-  for(size_t i = 0; i < LOOPS; i++) {
+  for(unsigned int i = 0; i < LOOPS; i++) {
     root = 0.5 * (root + (* a / root));
   }
   // std::cout << "Sqrt1 of " << * a << " is: " << root << std::endl;
@@ -21,13 +20,13 @@ float sqrt1(float * a) {
 
 template <size_t LOOPS = 2>
 void sqrt2(float * __restrict__ a, float * __restrict__ root) {
-  for(size_t i = 0; i < 4; i++) {
-    int * ai = reinterpret_cast<int *>(&a[i]);
-    int * initial = reinterpret_cast<int *>(&root[i]);
-    * initial = (1 << 29) + (* ai >> 1) - (1 << 22) - 0x4C000;
-    root[i] = * reinterpret_cast<float *>(initial);
-  }
-  for(size_t j = 0; j < LOOPS; j++) {
+  int * ai = reinterpret_cast<int *>(a);
+  int * initial = reinterpret_cast<int *>(root);
+  initial[0] = (1 << 29) + (ai[0] >> 1) - (1 << 22) - 0x4C000;
+  initial[1] = (1 << 29) + (ai[1] >> 1) - (1 << 22) - 0x4C000;
+  initial[2] = (1 << 29) + (ai[2] >> 1) - (1 << 22) - 0x4C000;
+  initial[3] = (1 << 29) + (ai[3] >> 1) - (1 << 22) - 0x4C000;
+  for(unsigned int j = 0; j < LOOPS; j++) {
     root[0] = 0.5 * (root[0] + (a[0] / root[0]));
     root[1] = 0.5 * (root[1] + (a[1] / root[1]));
     root[2] = 0.5 * (root[2] + (a[2] / root[2]));
@@ -42,8 +41,7 @@ void v4sf_sqrt(v4sf * __restrict__ a, v4sf * __restrict__ root) {
   v4si * ai = reinterpret_cast<v4si *>(a);
   v4si * initial = reinterpret_cast<v4si *>(root);
   * initial = (1 << 29) + (* ai >> 1) - (1 << 22) - 0x4C000;
-  root = reinterpret_cast<v4sf *>(initial);
-  for (size_t i = 0; i < LOOPS; i++) {
+  for (unsigned int i = 0; i < LOOPS; i++) {
     * root = 0.5 * (* root + (* a / * root));
   }
   // float * aF = reinterpret_cast<float *>(a);
